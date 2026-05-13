d49bfef09fb6805662fa3d66b9a9b451c4e4d9a3
vitabravol2-ctrl <vitabravol2@gmail.com>
2026-05-13 17:34:54 +0300

    Default startup to Conveyor mode with micro-grid flag

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from app.gui.micro_grid_window import MicroGridWindow
 from app.gui.mode_selector import ModeSelectorDialog
 
 
+ENABLE_MICRO_GRID_MODE = False
+
+
 def main() -> None:
     app = QApplication([])
-    selector = ModeSelectorDialog()
-    if selector.exec() != selector.DialogCode.Accepted:
-        return
-    if selector.selected_mode == ModeSelectorDialog.MICRO_GRID_MODE:
-        window = MicroGridWindow()
+
+    if ENABLE_MICRO_GRID_MODE:
+        selector = ModeSelectorDialog()
+        if selector.exec() != selector.DialogCode.Accepted:
+            return
+        if selector.selected_mode == ModeSelectorDialog.MICRO_GRID_MODE:
+            window = MicroGridWindow()
+        else:
+            window = MainWindow()
     else:
         window = MainWindow()
+
     window.show()
     app.exec()
 
